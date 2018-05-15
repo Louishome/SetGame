cdbab9512334d09eca25235ccff74c114955b923
Yeh, Louis <Louis.Yeh@garmin.com>
2018-05-15 09:34:00 +0800

    Create init of SetGame (not tested)

--- SetGame/SetGame/SetGame.swift
+++ SetGame/SetGame/SetGame.swift
@@ -9,24 +9,56 @@
 import Foundation
 
 struct SetGame {
-    let numberOfFeatureType = 4
-    let numberOfFeatureValue = FeatureValue.count
-    let numberOfTotalCard = 3 ^^ 4
-    let numberOfCardInASet = 3
-    var acceptableBitwiseValues:[Int] {
+    public static let numberOfFeatureType = Card.cardFeatureTypes.count
+    public static let numberOfFeatureValue = FeatureValue.count
+    public static let numberOfTotalCard = numberOfFeatureValue ^^ numberOfFeatureType
+    public static let numberOfCardInASet = 3
+    public static let numberOfCardOnBoardInitial = 12
+    private var cardsOnBoard: [Card]
+    private var cardsInDeck: [Card]
+    private var selectedCards: [Card]
+
+    var acceptableBitwiseValues: [Int] {
         get {
-        var result = (0..<numberOfFeatureValue).map { FeatureValue(rawValue: $0)!.bitwise }
+        var result = (0..<SetGame.numberOfFeatureValue).map { FeatureValue(rawValue: $0)!.bitwise }
             result.append(result.reduce (0) { $0 | $1 })
             return result
         }
     }
     func isSet(cards: [Card]) -> Bool {
-        guard (numberOfCardInASet == cards.count) else {
+        guard (SetGame.numberOfCardInASet == cards.count) else {
             return false
         }
         return Card.cardFeatureTypes.map { (n) -> [Feature] in cards.map { $0.cardFeatures.filter { $0.type == n }.first! } }.reduce (true) { $0 || acceptableBitwiseValues.contains($1.reduce (0) { $0 | $1.value.bitwise }) }
     }
 
+    init() {
+        cardsInDeck = [Card]()
+        cardsOnBoard = [Card]()
+        selectedCards = [Card]()
+        prepareAllCards(type: 0, value: 0, featuresBucket: [Feature](), deck: &cardsInDeck)
+        for _ in 0..<SetGame.numberOfCardOnBoardInitial {
+            if let rendomCard = cardsInDeck.draw() {
+                cardsOnBoard.append(rendomCard)
+            }
+        }
+    }
+
+    public func prepareAllCards(type: Int, value: Int, featuresBucket: [Feature], deck: inout [Card]) {
+        guard (type < SetGame.numberOfFeatureType) else {
+            if let newCard = Card(features: featuresBucket) {
+                deck.append(newCard)
+            }
+            return
+        }
+        for featureValue in 0..<SetGame.numberOfCardInASet {
+            var nextLevelBucket = featuresBucket
+            if let newFeatureValue = FeatureValue(rawValue: featureValue) {
+                nextLevelBucket.append(Feature(type: Card.cardFeatureTypes[type], value: newFeatureValue))
+            }
+            prepareAllCards(type: type + 1, value: featureValue, featuresBucket: nextLevelBucket, deck: &deck)
+        }
+    }
 }
 
 extension FeatureValue {
@@ -34,6 +66,15 @@ extension FeatureValue {
         get {
             return 2 ^^ self.rawValue
         }
+    }
+}
+
+extension Array {
+    public mutating func draw() -> Element? {
+        guard (self.count > 0) else {
+            return nil
+        }
+        return self.remove(at: Int(arc4random_uniform(UInt32(self.count))))
     }
 }
 
