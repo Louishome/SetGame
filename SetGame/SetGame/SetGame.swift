b21b1997ab88b2d52d80036db812a075857cd27f
Yeh, Louis <Louis.Yeh@garmin.com>
2018-05-24 15:52:01 +0800

    Finish set game with simple score system
    Players get scores as they find a set. The total score will be the number of matched card they found.

--- SetGame/SetGame/SetGame.swift
+++ SetGame/SetGame/SetGame.swift
@@ -15,11 +15,16 @@ struct SetGame {
     public static let numberOfCardInASet = 3
     public static let numberOfCardOnBoardInitial = 12
 
-    private var cards:[Card]
-    public var cardsUnselected: [Int]
-    public var cardsInDeck: [Int]
-    public var cardsSelected: [Int]
-    public var cardsMatched: [Int]
+    private var _cardsUnselected: [Int]
+    private var _cardsInDeck: [Int]
+    private var _cardsSelected: [Int]
+    private var _cardsMatched: [Int]
+
+    public let cards:[Card]
+    public var cardsUnselected: [Int] { get { return _cardsUnselected } }
+    public var cardsInDeck: [Int] { get { return _cardsInDeck } }
+    public var cardsSelected: [Int] { get { return _cardsSelected } }
+    public var cardsMatched: [Int] { get { return _cardsMatched } }
 
     var acceptableBitwiseValues: [Int] {
         get {
@@ -32,22 +37,66 @@ struct SetGame {
         guard (SetGame.numberOfCardInASet == cards.count) else {
             return false
         }
-        return Card.cardFeatureTypes.map { (n) -> [Feature] in cards.map { $0.cardFeatures.filter { $0.type == n }.first! } }.reduce (true) { $0 || acceptableBitwiseValues.contains($1.reduce (0) { $0 | $1.value.bitwise }) }
+        return Card.cardFeatureTypes.map { (n) -> [Feature] in cards.map { $0.cardFeatures.filter { $0.type == n }.first! } }.reduce (true) { $0 && acceptableBitwiseValues.contains($1.reduce (0) { $0 | $1.value.bitwise }) }
     }
 
-    init() {
-        cards = [Card]()
-        (cardsInDeck, cardsUnselected, cardsSelected, cardsMatched) = ([], [], [], [])
-        prepareAllCards(type: 0, value: 0, featuresBucket: [Feature](), deck: &cards)
-        cardsInDeck = Array(cards.indices)
-        for _ in 0..<SetGame.numberOfCardOnBoardInitial {
-            if let rendomCard = cardsInDeck.draw() {
-                cardsUnselected.append(rendomCard)
+    init(with numberOfCards: Int) {
+        var tempCards = [Card]()
+        SetGame.prepareAllCards(type: 0, value: 0, featuresBucket: [Feature](), deck: &tempCards)
+        cards = tempCards
+        (_cardsInDeck, _cardsUnselected, _cardsSelected, _cardsMatched) = ([], [], [], [])
+        _cardsInDeck = Array(cards.indices)
+        for _ in 0..<numberOfCards {
+            if let randomCard = _cardsInDeck.draw() {
+                _cardsUnselected.append(randomCard)
             }
         }
     }
+    public mutating func draw3Cards() -> [Int] {
+        checkSelectedCards()
+        var drawnCardIndexes = [Int]()
+        for _ in 0..<3 {
+            if let randomCard = _cardsInDeck.draw() {
+                _cardsUnselected.append(randomCard)
+                drawnCardIndexes.append(randomCard)
+            }
+        }
+        return drawnCardIndexes
+    }
+    private mutating func checkSelectedCards() -> Void {
+        guard _cardsSelected.count <= SetGame.numberOfCardInASet else {
+            print("Error: more than \(SetGame.numberOfCardInASet) cards seleted, will unselect them.")
+            _cardsSelected.moveAll(to: &_cardsUnselected)
+            return
+        }
+        guard _cardsSelected.count == SetGame.numberOfCardInASet else {
+            return
+        }
+        if isSet(cards: _cardsSelected.map { cards[$0] }) {
+            _cardsSelected.moveAll(to: &_cardsMatched)
+        } else {
+            _cardsSelected.moveAll(to: &_cardsUnselected)
+        }
+    }
 
-    public func prepareAllCards(type: Int, value: Int, featuresBucket: [Feature], deck: inout [Card]) {
+    public mutating func selectCard(of cardIndex:Int) {
+        guard !_cardsMatched.contains(cardIndex) else {
+            print("Error: selected a matched card.")
+            return
+        }
+        guard !_cardsSelected.contains(cardIndex) else {
+            _cardsSelected.move(this: cardIndex, to: &_cardsUnselected)
+            return
+        }
+        guard cardsUnselected.contains(cardIndex) else {
+            print("Error: cannot find this card id in unslected cards.")
+            return
+        }
+        checkSelectedCards()
+        _cardsUnselected.move(this: cardIndex, to: &_cardsSelected)
+    }
+
+    static private func prepareAllCards(type: Int, value: Int, featuresBucket: [Feature], deck: inout [Card]) {
         guard (type < SetGame.numberOfFeatureType) else {
             if let newCard = Card(features: featuresBucket) {
                 deck.append(newCard)
@@ -78,6 +127,20 @@ extension Array {
             return nil
         }
         return self.remove(at: Int(arc4random_uniform(UInt32(self.count))))
+    }
+}
+extension Array where Element: Equatable {
+    public mutating func move(this element:Element ,to anotherArray: inout [Element]) -> Void {
+        guard let indexOfElement = self.index(of: element) else {
+            print("Error: unable to move this element, because it is not in this array.")
+            return
+        }
+        anotherArray.append(self.remove(at: indexOfElement))
+    }
+    public mutating func moveAll(to anotherArrey: inout [Element]) -> Void {
+        while let firstElement = self.first {
+            self.move(this: firstElement, to: &anotherArrey)
+        }
     }
 }
 
--- SetGame/SetGame/ViewController.swift
+++ SetGame/SetGame/ViewController.swift
@@ -9,21 +9,86 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        resetGame()
+        updateUI()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBOutlet weak var scoreLable: UILabel!
     @IBOutlet var cardButtons: [UIButton]!
     @IBAction func touchCardButton(_ sender: UIButton) {
-
+        guard let cardIndex = cardButtonMapping[sender] else {
+            return
+        }
+        setGame.selectCard(of: cardIndex)
+        updateUI()
     }
+    @IBAction func touchNewGame(_ sender: UIButton) {
+        resetGame()
+        updateUI()
+    }
+    @IBAction func touchDraw3Cards(_ sender: UIButton) {
+        guard (cardButtons.filter({ cardButtonMapping[$0] == nil }).count >= 3) else {
+            print("No more room for new cards")
+            return
+        }
+        cardIndexesNeedAButton = setGame.draw3Cards()
+        updateUI()
+    }
+    private lazy var setGame = SetGame(with: SetGame.numberOfCardOnBoardInitial)
+    private var cardButtonMapping = [UIButton: Int]()
+    private var cardIndexesNeedAButton = [Int]()
 
+    private func updateUI() {
+        // Update mapping table
+            // Insert cards if there are cards need to be paired
+        while let cardIndex = cardIndexesNeedAButton.draw() {
+            if let buttonWithMatchedCard = getButtonWithMatchedCard() {
+                cardButtonMapping[buttonWithMatchedCard] = cardIndex
+                continue
+            }
+            if let buttonWithoutCard = getButtonWithoutCard() {
+                cardButtonMapping[buttonWithoutCard] = cardIndex
+                continue
+            }
+        }
+            // Remove pairs with matched cards
+        while let buttonWithMatchedCard = getButtonWithMatchedCard() {
+            cardButtonMapping[buttonWithMatchedCard] = nil
+        }
 
+        // Update button display
+        for button in cardButtons {
+            if let cardIndex = cardButtonMapping[button] {
+                // Checking the cards seleted or not (not sure whether this should be out of this loop)
+                if setGame.cardsSelected.contains(cardIndex) {
+                    button.layer.borderWidth = 3.0
+                    button.layer.borderColor = UIColor.blue.cgColor
+                } else {
+                    button.layer.borderWidth = 0
+                }
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+                button.setAttributedTitle(setGame.cards[cardIndex].cardPatternString, for: .normal)
+            } else {
+                button.layer.borderWidth = 0
+                button.setTitle("", for: .normal)
+                button.setAttributedTitle(NSAttributedString(string: ""), for: .normal)
+                button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0)
+            }
+        }
+        // Update score
+        scoreLable.text = "Score: \(setGame.cardsMatched.count)"
+    }
+    private func getButtonWithMatchedCard() -> UIButton? {
+        return cardButtonMapping.filter({ setGame.cardsMatched.contains($0.value) }).first?.key
+    }
+    private func getButtonWithoutCard() -> UIButton? {
+        return cardButtons.filter({ cardButtonMapping[$0] == nil }).first
+    }
+    private func resetGame() {
+        cardButtonMapping = [UIButton: Int]()
+        setGame = SetGame(with: SetGame.numberOfCardOnBoardInitial)
+        cardIndexesNeedAButton = setGame.cardsUnselected
+    }
 }
 
